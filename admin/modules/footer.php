a5b8b07b84b8ec888c202e14e7cfdbeca7eb979c
Nga9499 <59242136+Nga9499@users.noreply.github.com>
2020-12-01 10:35:42 +0700

    Update footer.php

--- admin/modules/footer.php
+++ admin/modules/footer.php
@@ -1,7 +1,7 @@
         <section>
         <div class="footer" style="position: fixed; bottom: 0px; width: 100%">
                 <div class="wthree-copyright" >
-                    <p>&copy; 2019 Thế giới laptop</p>
+                    <p>&copy; Thế giới laptop</p>
                 </div>
             </div>
         </section>
